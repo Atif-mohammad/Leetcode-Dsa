7a95e80aca059bc3df124f0ac8efb098cf1a7046
Atif-mohammad <145256781+Atif-mohammad@users.noreply.github.com>
2024-02-27 10:55:05 +0530

    Add files via upload

---
+++ merge_two_sorted_array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = nums1.length - 1;
+        
+        while (j >= 0 && i >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are remaining elements in nums2
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+
+        // Print the merged array nums1
+        // for (int val : nums1) {
+        //     System.out.print(val + " ");
+        // }
+    }
+}
